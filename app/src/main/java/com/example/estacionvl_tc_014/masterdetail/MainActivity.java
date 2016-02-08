f1d43ca6bf21841845e1c774dcfd2c19f405d66e
Dario Chamoro <dario.chamorro.v@gmail.com>
2016-02-08 10:27:51 -0500

    Soporte Multiscreen en recursos implementado

---
+++ app/src/main/java/com/example/estacionvl_tc_014/masterdetail/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.estacionvl_tc_014.masterdetail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_main);
+    }
+}
